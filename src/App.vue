3307b2c1bdc6299dd193a1613abb46b02b800e12
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-08 15:50:29 -0300

    Create Banner

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
+<script lang="ts">
+  import Banner from "./components/Banner.vue";
+  import ConteudoPrincipal from "./components/ConteudoPrincipal.vue";
+  
+
+  export default {
+    components: { Banner, ConteudoPrincipal }
+    
+  }
+</script>
 <template>
-  <header>
-    <div>
-      <img src="/src/assets/images/logo.svg" alt="logo do Cookin' Up" class="logo">
-      <h1 class="cabecalho"></h1>
-
-      <p class="cabecalho-lg frase-cabecalho">
-        <span class="texto-verde">Um banquete de ideias para</span>
-        despertar o chefe que há em você!
-      </p>
-      <p class="subtitulo-lg">
-        Explore novas receitas todos os dias com ingrediente que estão ao seu alcance!
-      </p>
-
-    </div>
-
-    <img src="/src/assets/images/foto-banner.png" alt="" class="foto-banner">
-  </header>
+  <Banner />
+  <ConteudoPrincipal />
+   
 </template>
+
+<style scoped>
+   
+
+</style>
