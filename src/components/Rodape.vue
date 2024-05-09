f951182cce4f30b176423db4817de84523eb341d
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-09 02:57:46 -0300

    Create Rodape.vue

---
+++ src/components/Rodape.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="rodape paragrafo">
+    Desenvolvido por Alura 2023 | Projeto sem fins lucrativos
+
+  </footer>
+
+</template>
+
+<style escoped>
+.rodape {
+  background: var(--verde-escuro, #263A29);
+  padding: 1.5rem 7.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@media only screen and (max-width: 1300px) {
+  .rodape {
+    padding: 1.5rem 3.75rem;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .rodape {
+    padding: 1.5rem;
+  }
+}
+</style>
